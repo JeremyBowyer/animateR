08039a54f2f30d6ea2f3586db19cdf1ddc27217a
jeremy <jeremybowyer@gmail.com>
2018-05-30 20:35:49 -0400

    Re-worked tabs on options page. Now the data changing tabs are grouped together.

--- ui-modules/ui-options.R
+++ ui-modules/ui-options.R
@@ -40,81 +40,86 @@ optionsPage <- function() {
               )
           ),
           tabPanel(
-              "Date Aggregation",
-              tags$div(
-                h3("Date Aggregation"),
-                conditionalPanel(
-                  condition = "output.aggCheck",
-                  actionLink("aggClear", "Undo Aggregation", style="color: #f12828;"),
-                  tags$br()
-                ),
-                selectInput("dateAggDateCol", "Select Date column", choices=list("", "Please upload data first.")),
-                textInput("dateAggDateColFormat", "Format dates are in (check 'Data Preview' tab)", "%m/%d/%Y"),
-                selectInput("groupByCols", "Select Category column(s) (optional)", multiple = TRUE, choices=list("", "Please upload data first.")),
-                selectInput("aggregationLevel", "Aggregation Level (your data must be more granular than selected level)", choices = aggregationLevelList),
-                selectInput("aggregationFunc", "Aggregation Function", choices = aggregationFuncList),
-                conditionalPanel(
-                  condition = "!output.aggCheck",
-                  actionButton("aggregateData", "Aggregate Data")
+            "Change Data",
+            tabsetPanel(
+              tabPanel(
+                "Date Aggregation",
+                tags$div(
+                  h3("Date Aggregation"),
+                  conditionalPanel(
+                    condition = "output.aggCheck",
+                    actionLink("aggClear", "Undo Aggregation", style="color: #f12828;"),
+                    tags$br()
                   ),
-                tags$br(),
-                style="padding: 5px 20px 20px 20px; background: #e4dfd6; border: 1px solid #b5b3b0; margin: 10px 0 0 0; border-radius: 5px;"
-              )
-            ),
-          tabPanel(
-            "Filters",
-            tags$div(
-              h3("Filters"),
-              tags$p("Warning: Filtering by one column will be applied to entire dataset, and will affect all subsequent analysis, including Metric Dive tab."),
-              conditionalPanel(
-                condition = "output.filtersCheck",
-                actionButton("applyFilters", "Apply Filters", icon("filter"), style="padding: 5px 10px 5px 10px;"),
-                tags$br(),
-                actionLink("filterClear", "Clear All Filters", style="color: #f12828;"),
+                  selectInput("dateAggDateCol", "Select Date column", choices=list("", "Please upload data first.")),
+                  textInput("dateAggDateColFormat", "Format dates are in (check 'Data Preview' tab)", "%m/%d/%Y"),
+                  selectInput("groupByCols", "Select Category column(s) (optional)", multiple = TRUE, choices=list("", "Please upload data first.")),
+                  selectInput("aggregationLevel", "Aggregation Level (your data must be more granular than selected level)", choices = aggregationLevelList),
+                  selectInput("aggregationFunc", "Aggregation Function", choices = aggregationFuncList),
+                  conditionalPanel(
+                    condition = "!output.aggCheck",
+                    actionButton("aggregateData", "Aggregate Data")
+                  ),
+                  tags$br(),
+                  style="padding: 5px 20px 20px 20px; background: #e4dfd6; border: 1px solid #b5b3b0; margin: 10px 0 0 0; border-radius: 5px;"
+                )
+              ),
+              tabPanel(
+                "Filters",
+                tags$div(
+                  h3("Filters"),
+                  tags$p("Warning: Filtering by one column will be applied to entire dataset, and will affect all subsequent analysis, including Metric Dive tab."),
+                  conditionalPanel(
+                    condition = "output.filtersCheck",
+                    actionButton("applyFilters", "Apply Filters", icon("filter"), style="padding: 5px 10px 5px 10px;"),
+                    tags$br(),
+                    actionLink("filterClear", "Clear All Filters", style="color: #f12828;"),
+                    tags$hr()
+                  ),
+                  tags$div(
+                    selectInput("filterSelected", label = NULL, width = "275px", choices = filterList),
+                    style= "display:inline-block; vertical-align: top;"
+                  ),
+                  tags$div(actionButton("addFilter", "Add Filter", style="padding:4px;font-size: 75%;"),style="display:inline-block; vertical-align: top;"),
+                  tags$div(id="filters"),
+                  id="filter-container", style="padding: 5px 20px 20px 20px; background: #e4dfd6; border: 1px solid #b5b3b0; margin: 10px 0 0 0; border-radius: 5px;"
+                ),
                 tags$hr()
-                ),
-              tags$div(
-                selectInput("filterSelected", label = NULL, width = "275px", choices = filterList),
-                style= "display:inline-block; vertical-align: top;"
-                ),
-              tags$div(actionButton("addFilter", "Add Filter", style="padding:4px;font-size: 75%;"),style="display:inline-block; vertical-align: top;"),
-              tags$div(id="filters"),
-              id="filter-container", style="padding: 5px 20px 20px 20px; background: #e4dfd6; border: 1px solid #b5b3b0; margin: 10px 0 0 0; border-radius: 5px;"
               ),
-            tags$hr()
-            ),
-          tabPanel(
-            "Transformations",
-            tags$div(
-              h3("Metric Transformations"),
-              conditionalPanel(
-                condition = "output.transformationsCheck",
-                actionButton("applyTransformations", "Create Transformations", icon("recycle"), style="padding: 5px 10px 5px 10px;"),
-                tags$br(),
-                actionLink("transformationsClear", "Clear All Transformations", style="color: #f12828;")
-                ),
-              tags$div(
-                selectInput(
-                  "transformationselected",
-                  label = NULL,
-                  width = "275px",
-                  choices = transformationList),
-                style= "display:inline-block; vertical-align: top;"
-                ),
-              tags$div(
-                actionButton(
-                  "addTransformation",
-                  "Add Transformation",
-                  style="padding:4px;font-size: 75%;"
+              tabPanel(
+                "Transformations",
+                tags$div(
+                  h3("Metric Transformations"),
+                  conditionalPanel(
+                    condition = "output.transformationsCheck",
+                    actionButton("applyTransformations", "Create Transformations", icon("recycle"), style="padding: 5px 10px 5px 10px;"),
+                    tags$br(),
+                    actionLink("transformationsClear", "Clear All Transformations", style="color: #f12828;")
                   ),
-                style="display:inline-block; vertical-align: top;"
+                  tags$div(
+                    selectInput(
+                      "transformationselected",
+                      label = NULL,
+                      width = "275px",
+                      choices = transformationList),
+                    style= "display:inline-block; vertical-align: top;"
+                  ),
+                  tags$div(
+                    actionButton(
+                      "addTransformation",
+                      "Add Transformation",
+                      style="padding:4px;font-size: 75%;"
+                    ),
+                    style="display:inline-block; vertical-align: top;"
+                  ),
+                  tags$div(id="transformations"),
+                  id="transformation-container",
+                  style="padding: 5px 20px 20px 20px; background: #e4dfd6; border: 1px solid #b5b3b0; margin: 10px 0 0 0; border-radius: 5px;"
                 ),
-              tags$div(id="transformations"),
-              id="transformation-container",
-              style="padding: 5px 20px 20px 20px; background: #e4dfd6; border: 1px solid #b5b3b0; margin: 10px 0 0 0; border-radius: 5px;"
-              ),
-            tags$hr()
+                tags$hr()
+              )
             )
+          )
           ),
         tags$hr(),
         actionButton("drawChart", "Draw Chart", style="color: #fff; background-color: rgb(2, 140, 7); border: solid 1px #005a03;"),
